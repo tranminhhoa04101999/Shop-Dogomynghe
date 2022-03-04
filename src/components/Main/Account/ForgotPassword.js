f17200d9f9a1a673024072cceadb4c01d69660cb
hoa <tranminhhoa04101999@gmail.com>
2022-03-04 20:33:23 +0700

    rate

--- src/components/Main/Account/ForgotPassword.js
+++ src/components/Main/Account/ForgotPassword.js
@@ -1,18 +1,80 @@
 import React, { useState } from 'react';
 import './ForgotPassword.css';
-import { Input, Button, Card } from 'antd';
+import { Input, Button, Card, notification } from 'antd';
+import { LINKCONNECT_BASE } from '../../../App';
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState('');
+  const [loaded, setLoaded] = useState(false);
+  const openNotificationWithIcon = (props) => {
+    notification[props.type]({
+      message: props.message,
+      description: props.desc,
+    });
+  };
+  const emailOnchangeHandler = (props) => {
+    setEmail(props.value);
+  };
 
+  const btnSubmitHandler = async () => {
+    setLoaded(true);
+    if (email === '') {
+      openNotificationWithIcon({
+        type: 'warning',
+        message: 'email trống',
+        desc: 'Vui lòng điền email',
+      });
+      return;
+    } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      openNotificationWithIcon({
+        type: 'warning',
+        message: 'email không hợp lệ',
+        desc: 'Vui lòng điền lại email',
+      });
+      return;
+    }
+    await fetch(`${LINKCONNECT_BASE}/getforgotPassword?email=${email}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data === 1) {
+          openNotificationWithIcon({
+            type: 'success',
+            message: 'Thay đổi thành công',
+            desc: 'Vui lòng check mail',
+          });
+        } else if (data === 0) {
+          openNotificationWithIcon({
+            type: 'error',
+            message: 'Lấy mật khẩu thất bại',
+            desc: '',
+          });
+        } else if (data === 2) {
+          openNotificationWithIcon({
+            type: 'error',
+            message: 'Email không tồn tại',
+            desc: '',
+          });
+        }
+      });
+    setLoaded(false);
+  };
   return (
     <div className="grid wide container-forgotpass">
       <div className="row">
         <div className="col l-4"></div>
         <div className="col l-4">
           <Card>
-            <Input placeholder="Nhập email để lấy lại mật khẩu" />
-            <Button className="forgotpass-btn">Gửi</Button>
+            <Input
+              placeholder="Nhập email để lấy lại mật khẩu"
+              onChange={(event) => emailOnchangeHandler({ value: event.target.value })}
+            />
+            <Button
+              loading={loaded === true ? 1 : 0}
+              className="forgotpass-btn"
+              onClick={() => btnSubmitHandler()}
+            >
+              Gửi
+            </Button>
           </Card>
         </div>
         <div className="col l-4"></div>
--- src/components/Main/Account/OrderHistory.js
+++ src/components/Main/Account/OrderHistory.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './OrderHistory.css';
-import { Card, Image, Tabs } from 'antd';
+import { Card, Image, Tabs, Button, Popconfirm, notification, Input } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleNotch, faTruck } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faTruck } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStarEm } from '@fortawesome/free-regular-svg-icons';
+
 import { LINKCONNECT_BASE, LINKIMG_BASE } from '../../../App';
+import Rating from 'react-rating';
 import moment from 'moment';
 const gridStyle = {
   width: '100%',
@@ -50,11 +53,23 @@ const INITIAL_DATA = {
     },
   ],
 };
+
+const INITIAL_RATEADD = {
+  idOrder: 0,
+  idProduct: 0,
+  idCustomer: 0,
+  descRate: '',
+  pointRate: 0,
+};
 const { TabPane } = Tabs;
 const OrderHistory = () => {
   const [dataOrder, setDataOrder] = useState([INITIAL_DATA]);
   const [idStatus, setIdStatus] = useState(0);
   const dataLogined = JSON.parse(localStorage.getItem('infoLogined'));
+  const [showModal, setShowModal] = useState(false);
+  const [dataRateCurrent, setDataRateCurrent] = useState(null);
+  const [dataRateAdd, setDataRateAdd] = useState([INITIAL_RATEADD]);
+  const [loading, setloading] = useState(false);
   useEffect(() => {
     if (dataLogined !== null) {
       fetch(
@@ -64,9 +79,78 @@ const OrderHistory = () => {
         .then((data) => setDataOrder(data));
     }
   }, []);
+  const openNotificationWithIcon = (props) => {
+    notification[props.type]({
+      message: props.message,
+      description: props.desc,
+    });
+  };
+  useEffect(() => {
+    if (dataLogined !== null) {
+      fetch(
+        `${LINKCONNECT_BASE}/searchOrderByIdAccount?idAccount=${dataLogined.idAccount}`
+      )
+        .then((response) => response.json())
+        .then((data) => setDataOrder(data));
+    }
+  }, [loading]);
 
-  useEffect(() => {}, [idStatus]);
+  const btnHuyDonHandler = async (props) => {
+    setloading(true);
+    await fetch(`${LINKCONNECT_BASE}/huyOrder?idOrder=${props.idOrder}`, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+        Accepts: '*/*',
 
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data === 1) {
+          openNotificationWithIcon({
+            type: 'success',
+            message: 'Hủy đơn hàng thành công',
+            desc: '',
+          });
+        } else if (data === 0) {
+          openNotificationWithIcon({
+            type: 'error',
+            message: 'Hủy đơn hàng thất bại',
+            desc: '',
+          });
+        }
+      });
+    setloading(false);
+  };
+  const btnRateHandler = (props) => {
+    setDataRateCurrent({
+      idOrder: props.idOrder,
+      productSearchResponses: props.productList,
+      customer: props.customer,
+    });
+    var dataAdd = [];
+    props.productList.map((item) => {
+      dataAdd = [
+        ...dataAdd,
+        {
+          idOrder: props.idOrder,
+          idProduct: item.idProduct,
+          idCustomer: props.customer.idCustomer,
+          descRate: '',
+          pointRate: 0,
+        },
+      ];
+      setDataRateAdd(dataAdd);
+    });
+    setShowModal(true);
+  };
   function RenderWithKey(props) {
     if (props.idStatus > 6) {
       return (
@@ -114,6 +198,39 @@ const OrderHistory = () => {
                         </span>
                       </div>
                     </div>
+                    {itemOrder.orders.status.idStatus === 1 && (
+                      <div>
+                        <Popconfirm
+                          title="Bạn muốn xóa?"
+                          onConfirm={() =>
+                            btnHuyDonHandler({
+                              idOrder: itemOrder.orders.idOrder,
+                            })
+                          }
+                        >
+                          <Button type="primary" danger loading={loading ? 1 : 0}>
+                            <p style={{ margin: 0, fontSize: '1.4rem' }}>Hủy đơn</p>
+                          </Button>
+                        </Popconfirm>
+                      </div>
+                    )}
+                    {itemOrder.orders.status.idStatus === 5 && (
+                      <div>
+                        <Button
+                          type="primary"
+                          danger
+                          onClick={() =>
+                            btnRateHandler({
+                              idOrder: itemOrder.orders.idOrder,
+                              productList: itemOrder.productSearchResponses,
+                              customer: itemOrder.orders.customer,
+                            })
+                          }
+                        >
+                          <p style={{ margin: 0, fontSize: '1.4rem' }}>Đánh giá</p>
+                        </Button>
+                      </div>
+                    )}
                   </div>
                   <div className="history-listproduct">
                     {itemOrder.productSearchResponses.map((itemProd, indexProd) => (
@@ -204,6 +321,39 @@ const OrderHistory = () => {
                           </span>
                         </div>
                       </div>
+                      {itemOrder.orders.status.idStatus === 1 && (
+                        <div>
+                          <Popconfirm
+                            title="Bạn muốn xóa?"
+                            onConfirm={() =>
+                              btnHuyDonHandler({
+                                idOrder: itemOrder.orders.idOrder,
+                              })
+                            }
+                          >
+                            <Button type="primary" danger loading={loading ? 1 : 0}>
+                              <p style={{ margin: 0, fontSize: '1.4rem' }}>Hủy đơn</p>
+                            </Button>
+                          </Popconfirm>
+                        </div>
+                      )}
+                      {itemOrder.orders.status.idStatus === 5 && (
+                        <div>
+                          <Button
+                            type="primary"
+                            danger
+                            onClick={() =>
+                              btnRateHandler({
+                                idOrder: itemOrder.orders.idOrder,
+                                productList: itemOrder.productSearchResponses,
+                                customer: itemOrder.orders.customer,
+                              })
+                            }
+                          >
+                            <p style={{ margin: 0, fontSize: '1.4rem' }}>Đánh giá</p>
+                          </Button>
+                        </div>
+                      )}
                     </div>
                     <div className="history-listproduct">
                       {itemOrder.productSearchResponses.map((itemProd, indexProd) => (
@@ -250,10 +400,27 @@ const OrderHistory = () => {
   function callback(key) {
     setIdStatus(key);
   }
+  const rateOnClickHandler = (props) => {
+    let data = dataRateAdd;
+    data = dataRateAdd.map((item) =>
+      item.idProduct === props.idProduct ? { ...item, pointRate: props.value } : item
+    );
+    setDataRateAdd(data);
+  };
+
+  const btnSubmitRateHandler = () => {
+    console.log('dataRateAdd', dataRateAdd);
+  };
+  const inputRateOnchangeHandler = (props) => {
+    let data = dataRateAdd;
+    data = dataRateAdd.map((item) =>
+      item.idProduct === props.idProduct ? { ...item, descRate: props.value } : item
+    );
+    setDataRateAdd(data);
+  };
 
   return (
     <div>
-      {}
       <Tabs defaultActiveKey="1" onChange={callback}>
         <TabPane tab="Tất cả" key="7">
           <RenderWithKey idStatus={7} />
@@ -274,6 +441,88 @@ const OrderHistory = () => {
           <RenderWithKey idStatus={6} />
         </TabPane>
       </Tabs>
+      <div className="modal" style={{ display: showModal ? 'flex' : 'none' }}>
+        <div className="modal-overlay" onClick={() => setShowModal(false)}></div>
+        <div className="modal-body">
+          {dataRateCurrent !== null && (
+            <div className="rate-form">
+              <div className="rate-header">
+                <Card>
+                  {dataRateCurrent.productSearchResponses.map((item, index) => {
+                    let indexRate = dataRateAdd.findIndex(
+                      (item1) => item1.idProduct === item.idProduct
+                    );
+                    return (
+                      <Card.Grid key={index} style={gridStyle}>
+                        <div className="history-listproduct__card">
+                          <div className="history-listproduct__card-Right">
+                            <Image
+                              width={120}
+                              height={100}
+                              src={`${LINKIMG_BASE}${item.imgURL}.jpg?alt=media`}
+                            />
+                            <div className="history-listproduct__card-name">
+                              <p className="history-listproduct__card-nameProduc">
+                                {item.nameProduct}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                        <Rating
+                          initialRating={dataRateAdd[indexRate].pointRate}
+                          emptySymbol={
+                            <FontAwesomeIcon
+                              icon={faStarEm}
+                              size="3x"
+                              style={{ color: '#ffb91e' }}
+                            />
+                          }
+                          fullSymbol={
+                            <FontAwesomeIcon
+                              icon={faStar}
+                              size="3x"
+                              style={{ color: '#ffc31a ' }}
+                            />
+                          }
+                          onClick={(value) =>
+                            rateOnClickHandler({
+                              value: value,
+                              idProduct: item.idProduct,
+                            })
+                          }
+                        />
+                        <Input.TextArea
+                          rows={4}
+                          placeholder="Thêm bình luận"
+                          maxLength={200}
+                          style={{ margin: '10px 0' }}
+                          onChange={(event) =>
+                            inputRateOnchangeHandler({
+                              value: event.target.value,
+                              idProduct: item.idProduct,
+                            })
+                          }
+                          value={dataRateAdd[indexRate].descRate}
+                        />
+                      </Card.Grid>
+                    );
+                  })}
+                </Card>
+              </div>
+            </div>
+          )}
+          <div style={{ float: 'right' }}>
+            <Button
+              style={{ margin: '10px 0' }}
+              type="primary"
+              danger
+              onClick={() => btnSubmitRateHandler()}
+            >
+              <p style={{ fontSize: '1.4rem', margin: 0 }}>Hoàn thành</p>
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
